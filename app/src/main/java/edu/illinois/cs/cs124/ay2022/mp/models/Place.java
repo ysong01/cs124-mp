73e369e496356ef09087f864df310008559601f4
Youngjin Song <ys62@illinois.edu>
2022-11-04 18:36:22 +1300

    For test 1, mp1

--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/models/Place.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/models/Place.java
@@ -1,6 +1,12 @@
 package edu.illinois.cs.cs124.ay2022.mp.models;
 
+import java.util.ArrayList;
+
+
 import java.util.List;
+
+
+
 
 /*
  * Model storing information about a place retrieved from the backend server.
@@ -31,9 +37,60 @@ public final class Place {
   // ID of the place
   private String id;
 
-  public static List<Place> search(final List<Place> places, final String searchInput) {
-    assert false;
-    return null;
+  public static List<Place> search(final List<Place> places, final String searchInput)  {
+
+    if (places == null || searchInput == null) {
+      throw new IllegalArgumentException();
+    }
+    if (places.size() == 0 || searchInput.isEmpty() || searchInput.isBlank()) {
+      return places;
+    }
+
+    List<Place> newList = new ArrayList<>();
+
+    String newSearch = searchInput.trim();
+
+    for (int k = 0; k < places.size(); k++) {
+      Place test = places.get(k);
+      String one = test.getDescription();
+      String two = one.replace('.', ' ');
+      String three = two.replace('!', ' ');
+      String four = three.replace('?', ' ');
+      String five = four.replace(',', ' ');
+      String six = five.replace(':', ' ');
+      String seven = six.replace(';', ' ');
+      String eight = seven.replace('/', ' ');
+
+
+      char[] ch = eight.toCharArray();
+      //char[] newCh = new char[];
+      int index = 0;
+      String ten = "";
+      for (int i = 0; i < ch.length; i++) {
+        if (Character.isDigit(ch[i]) || Character.isAlphabetic(ch[i]) || Character.isWhitespace(ch[i])) {
+          ten += ch[i];
+          //newCh[index] = ch[i];
+          index++;
+
+        }
+
+      }
+      String get = ten;
+      String[] get1 = get.split(" ");
+
+      for (int i = 0; i < get1.length; i++) {
+        System.out.println(get1[i]);
+        if (get1[i].equalsIgnoreCase(newSearch)) {
+          newList.add(places.get(k));
+          break;
+        }
+      }
+
+
+    }
+
+
+    return newList;
   }
 
   public String getId() {
